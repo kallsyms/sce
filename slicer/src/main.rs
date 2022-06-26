6f6e2055e32a2760914318efe5523a2aab26d362
Nick Gregory <computerfreak97@gmail.com>
2022-06-26 18:41:29 -0400

    more

--- slicer/src/main.rs
+++ slicer/src/main.rs
@@ -1,6 +1,7 @@
-use std::path::Path;
+use std::cell::RefCell;
 use std::collections::HashSet;
 use std::hash::{Hash, Hasher};
+use std::path::Path;
 use tree_sitter;
 use tree_sitter_c;
 
@@ -24,7 +25,7 @@ struct SlicerConfig {
     /// e.g. ("assignment_expression", ("left", "right"))
     propagating_types: Vec<(&'static str, (&'static str, &'static str))>,
 
-    /// Type names representing statements. Can use inheritance information from node-types.
+    /// Type names representing statements. Can use "inheritance" information from node-types.
     statement_types: Vec<&'static str>,
 
     /// Type names representing scopes in which we can slice (just functions?)
@@ -49,10 +50,10 @@ fn from_guessed_language(language: guess_language::Language) -> Option<SlicerCon
                 propagating_types: vec![
                     ("assignment_expression", ("left", "right")),
                 ],
-                statement_types: vec!["_statement"],
+                // TODO: from node-types.json
+                statement_types: vec!["expression_statement", "assignment_expression", "init_declarator"],
                 slice_scope_types: vec!["function_definition"],
-                var_definition_scope_types: vec!["compound_statement"], // TODO: is there a block
-                                                                        // type in node-types?
+                var_definition_scope_types: vec!["compound_statement"],
             })
         }
         _ => None
@@ -78,12 +79,13 @@ impl<'a> Eq for NameRef<'a> {}
 
 impl<'a> Hash for NameRef<'a> {
     fn hash<H: Hasher>(&self, state: &mut H) {
+        // we don't care about the node itself, just the name
         self.components.hash(state);
     }
 }
 
 impl<'a> NameRef<'a> {
-    fn affects(&self, other: NameRef) -> bool {
+    fn affects(&self, other: &NameRef) -> bool {
         let len = self.components.len().min(other.components.len());
         return self.components[..len].iter().zip(other.components[..len].iter()).all(|(a, b)| a == b);
     }
@@ -140,11 +142,18 @@ impl<'a> Iterator for DepthFirstWalk<'a> {
 
 impl<'a> DepthFirstWalk<'a> {
     /// Call the given cb for each node, skipping any descendants of a given node if the cb returns
-    /// false.
-    fn traverse<F>(&mut self, mut cb: F) where F: FnMut(tree_sitter::Node<'a>) -> bool {
+    /// false. Additionally, call on_descent when descending down into a new "layer" and on_ascent
+    /// when coming back up.
+    fn traverse_with_depth<F, D, A>(&mut self, mut cb: F, mut on_descent: D, mut on_ascent: A)
+        where F: FnMut(tree_sitter::Node<'a>) -> bool,
+              D: FnMut(tree_sitter::Node<'a>, tree_sitter::Node<'a>),
+              A: FnMut(tree_sitter::Node<'a>, tree_sitter::Node<'a>)
+              {
         'outer: loop {
-            if cb(self.cursor.node()) {
+            let mut node = self.cursor.node();
+            if cb(node) {
                 if self.cursor.goto_first_child() {
+                    on_descent(node, self.cursor.node());
                     continue;
                 }
             }
@@ -155,8 +164,11 @@ impl<'a> DepthFirstWalk<'a> {
 
             loop {
                 self.cursor.goto_parent();
+                on_ascent(node, self.cursor.node());
 
-                if self.cursor.node() == self.root {
+                node = self.cursor.node();
+
+                if node == self.root {
                     return;
                 }
 
@@ -165,6 +177,12 @@ impl<'a> DepthFirstWalk<'a> {
                 }
             }
         }
+    }
+
+    /// Call the given cb for each node, skipping any descendants of a given node if the cb returns
+    /// false.
+    fn traverse<F>(&mut self, cb: F) where F: FnMut(tree_sitter::Node<'a>) -> bool {
+        self.traverse_with_depth(cb, |_, _|{}, |_, _|{})
     }
 }
 
@@ -213,7 +231,120 @@ impl Slicer {
         names
     }
 
-    fn propagate_targets<'a>(&self, outer_scope: tree_sitter::Node, target_names: HashSet<NameRef>) {
+    /// Propagate the set of target names out through all assignments
+    fn propagate_targets<'a>(&self, outer_scope: tree_sitter::Node<'a>, initial_target_names: &HashSet<NameRef<'a>>) -> HashSet<NameRef<'a>> {
+        let mut target_names = initial_target_names.clone();
+
+        // TODO: use depth_first.traverse_with_depth to push and pop scopes based on
+        // var_definition_scope_types
+        loop {
+            let mut changed = false;
+
+            for descendant in depth_first(outer_scope) {
+                if let Some((_, (target_child_name, source_child_name))) = self.config.propagating_types.iter().find(|&&(expr_kind, (_, _))| expr_kind == descendant.kind()) {
+                    let target_node = descendant.child_by_field_name(target_child_name);
+                    let source_node = descendant.child_by_field_name(source_child_name);
+
+                    // Guard against things like python's `with` which may or may not have a target
+                    if target_node.is_none() || source_node.is_none() {
+                        continue;
+                    }
+
+                    let node_target_names = self.referenced_names(target_node.unwrap());
+                    let node_source_names = self.referenced_names(source_node.unwrap());
+
+                    // If any known targets "affects" a var in the source, all vars in the dest are now targets
+                    if target_names.iter().any(|tname| node_source_names.iter().any(|sname| tname.affects(&sname))) {
+                        println!("Propagating node {:?} adds {:?} to targets", descendant, node_target_names);
+                        changed = target_names.intersection(&HashSet::from_iter(node_target_names.iter().cloned())).count() > 0;
+                        target_names.extend(node_target_names);
+                    }
+                }
+            }
+            
+            if !changed {
+                break;
+            }
+        }
+
+        target_names
+    }
+
+    /// Returns an in-order Vec of the highest-level statement-type nodes which do not reference
+    /// any target name.
+    fn flatten_unreferenced<'a>(&self, target_func: tree_sitter::Node<'a>, target_names: &HashSet<NameRef<'a>>) -> Vec<tree_sitter::Node<'a>> {
+        let mut delete_nodes = vec![];
+
+        // compute set of each node which references any target.
+        // this is an equivalent to computing referenced names for each node in the walk,
+        // but avoids having to walk each subtree for every single node, instead doing the
+        // computation once and calling back in on_ascent.
+        let references = RefCell::new(HashSet::new());
+
+        depth_first(target_func).traverse_with_depth(
+            |descendant| {
+                if self.config.name_types.contains(&descendant.kind()) {
+                    let name = NameRef{node: descendant, components: self.name_components(descendant)};
+                    if target_names.contains(&name) {
+                        references.borrow_mut().insert(descendant);
+                    }
+                    return false;
+                }
+                return true;
+            },
+            |_, _|{},
+            |_, to| {
+                // "bubble-up" reference data when exiting out of a node
+                let mut cur = to.walk();
+                for child in to.children(&mut cur) {
+                    if references.borrow().get(&child).is_some() {
+                        references.borrow_mut().insert(to);
+                        break;
+                    }
+                }
+            }
+        );
+
+        depth_first(target_func).traverse(|statement| {
+            if !self.config.statement_types.contains(&statement.kind()) {
+                return true;
+            }
+
+            // if this node is not recorded as having a reference to a target, we can delete it
+            if !references.borrow().contains(&statement) {
+                // check if a parent has already been marked for deletion
+                let mut parent = statement;
+                let parent_deleted = loop {
+                    match parent.parent() {
+                        Some(n) => {
+                            parent = n;
+                        }
+                        None => {
+                            break false;
+                        }
+                    }
+
+                    if delete_nodes.contains(&parent) {
+                        break true;
+                    }
+                };
+
+                if !parent_deleted {
+                    delete_nodes.push(statement);
+                }
+            }
+            
+            // Have to continue descent because statement can mean both compound statements, single
+            // expression statements, etc.
+            return true;
+        });
+
+        delete_nodes
+    }
+
+    fn coalesce_nodes<'a>(&self, nodes: &Vec<tree_sitter::Node<'a>>) -> Vec<tree_sitter::Range> {
+        let ranges = vec![];
+        ranges
     }
 
     pub fn slice(&mut self, source_code: &str, target_point: tree_sitter::Point) -> String {
@@ -223,9 +354,10 @@ impl Slicer {
         let tree = parser.parse(source_code, None).unwrap();
         let root_node = tree.root_node();
 
-        let target = self.name_at_point(&root_node, target_point).unwrap();
+        let target_name = self.name_at_point(&root_node, target_point).unwrap();
 
-        let mut target_func = target.node;
+        // walk up to the containing function
+        let mut target_func = target_name.node;
         // Cursors don't do what you'd expect here?
         // cur = target.walk();
         // assert_eq!(cur.goto_parent(), true); fails
@@ -239,13 +371,13 @@ impl Slicer {
         };
 
         let mut target_names: HashSet<NameRef> = HashSet::new();
-        target_names.insert(target.clone());
+        target_names.insert(target_name.clone());
 
-        println!("{:?}", self.referenced_names(target_func));
+        target_names = self.propagate_targets(target_func, &target_names);
+        let delete_nodes = self.flatten_unreferenced(target_func, &target_names);
+        let delete_ranges = self.coalesce_nodes(&delete_nodes);
 
-        let deleted_nodes = self.propagate_targets(target_func, target_names);
-
-        return format!("{:?}", deleted_nodes);
+        return format!("{:?}", delete_ranges);
     }
 }
 
@@ -253,7 +385,10 @@ fn main() {
     let source_code = "#include <stdio.h>
 int main() {
     int x = 0;
-    s.y = 0;
+    int y = 0;
+    s.z = x;
+    foo = s;
+    foo.y = bar;
     return x;
 }";
 
@@ -263,7 +398,7 @@ int main() {
         config: slicer_config,
         src: String::from(source_code),
     };
-    let reduced = slicer.slice(source_code, tree_sitter::Point::new(3, 6));
+    let reduced = slicer.slice(source_code, tree_sitter::Point::new(2, 8));
 
     println!("{}", reduced);
 }
