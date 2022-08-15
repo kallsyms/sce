07c98f433c0802b68bd4e6d9bf635f82281eb7db
Nick Gregory <computerfreak97@gmail.com>
2022-08-14 21:13:35 -0400

    fix code extension

--- vscode/source-slicer/src/extension.ts
+++ vscode/source-slicer/src/extension.ts
@@ -1,12 +1,15 @@
 import * as vscode from 'vscode';
 import * as child_process from 'child_process';
 
-const SLICER_BIN = "/tmp/slicer";
+const SLICER_BIN = __dirname + "/../../../slicer/target/debug/slicer";
+
+type SliceDirection = "Backward" | "Forward";
 
 type SlicerRequest = {
     filename: string;
     content: string;
     point: [number, number];
+    direction: SliceDirection;
 };
 
 type SlicerResponse = {
@@ -14,61 +17,78 @@ type SlicerResponse = {
     point: [number, number];
 };
 
-const runSlicer = (request: SlicerRequest): Promise<SlicerResponse> => {
+function runSlicer(request: SlicerRequest): Promise<SlicerResponse> {
     return new Promise((resolve, reject) => {
         const proc = child_process.spawn(SLICER_BIN);
-        proc.stdin.write(JSON.stringify(request));
-        proc.stdin.end();
+        proc.on('error', (err) => {
+            reject(err);
+        }).on('exit', (code) => {
+            if (code !== 0) {
+                reject(new Error(`Slicer exited with code ${code}`));
+            }
+        }).on('close', (code) => {
+            if (code !== 0) {
+                reject(new Error(`Slicer exited with code ${code}`));
+            }
+        });
         proc.stdout.on('data', (data) => {
             const response = JSON.parse(data.toString());
             resolve(response);
         });
         proc.stderr.on('data', (data) => {
-            reject(data.toString());
+            reject(new Error(data.toString()));
         });
-    }
-    );
+
+        proc.stdin.write(JSON.stringify(request));
+        proc.stdin.end();
+    });
 };
 
+async function slice(direction: SliceDirection) {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return;
+    }
+
+    const filename = editor.document.fileName;
+    const content = editor.document.getText();
+    const language = editor.document.languageId;
+    const point = editor.selection.active;
+
+    const req: SlicerRequest = {
+        filename,
+        content,
+        point: [point.line, point.character],
+        direction: direction,
+    };
+
+    let resp: SlicerResponse;
+    try {
+        resp = await runSlicer(req);
+    } catch (e) {
+        await vscode.window.showErrorMessage("Error slicing: " + (e as Error).toString());
+        return;
+    }
+
+    const sliceDoc = await vscode.workspace.openTextDocument({
+        language: language,
+        content: resp.content,
+    });
+    // open the document as a preview
+    await vscode.window.showTextDocument(sliceDoc, {
+        preview: true,
+        selection: new vscode.Range(resp.point[0], resp.point[1], resp.point[0], resp.point[1]),
+        viewColumn: vscode.ViewColumn.Beside,
+    });
+}
+
 export function activate(context: vscode.ExtensionContext) {
-	let sliceCommand = vscode.commands.registerCommand('source-slicer.slice', async () => {
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            return;
-        }
-
-        const filename = editor.document.fileName;
-        const content = editor.document.getText();
-        const language = editor.document.languageId;
-        const point = editor.selection.active;
-
-        const req: SlicerRequest = {
-            filename,
-            content,
-            point: [point.line, point.character],
-        };
-
-        let resp: SlicerResponse;
-        try {
-            resp = await runSlicer(req);
-        } catch (e) {
-            vscode.window.showErrorMessage("Error slicing: ", e.toString());
-            return;
-        }
-
-        const sliceDoc = await vscode.workspace.openTextDocument({
-            language: language,
-            content: resp.content,
-        });
-        // open the document as a preview
-        vscode.window.showTextDocument(sliceDoc, {
-            preview: true,
-            selection: new vscode.Range(resp.point[0], resp.point[1], resp.point[0], resp.point[1]),
-            viewColumn: vscode.ViewColumn.Beside,
-        });
-	});
-
-	context.subscriptions.push(sliceCommand);
+	context.subscriptions.push(vscode.commands.registerCommand('source-slicer.sliceBackward', async () => {
+        await slice("Backward");
+	}));
+	context.subscriptions.push(vscode.commands.registerCommand('source-slicer.sliceForward', async () => {
+        await slice("Forward");
+	}));
 }
 
 export function deactivate() {}
